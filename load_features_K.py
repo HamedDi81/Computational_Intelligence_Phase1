161e45b22bff724a74eb13a8992655bd198867d2
kourosh47 <kourosh.sherkat@gmail.com>
2023-04-05 14:17:28 -0400

    my_code_load_features

---
+++ load_features_K.py
@@ -0,0 +1,42 @@
+import numpy as np
+from typing import Tuple
+
+def load_data(train_data: bool =True) -> Tuple[np.ndarray, np.ndarray]:
+  """
+  loads train/test features with image labels.  
+  """
+  if train_data:
+    data = np.load(f'/home/cyrus/features/train_data.npz')
+  else:
+    data = np.load(f'/home/cyrus/features/test_data.npz')
+
+  features = data['features']
+  img_labels = data['img_labels']
+  
+  return features, img_labels
+
+def load_data_with_domain_label() -> Tuple[np.ndarray, np.ndarray]:
+  """
+  loads portion of training features with domain label
+  """
+  data = np.load(f'/home/cyrus/features/train_data_w_label.npz')
+  train_features = data['features']
+  domain_labels = data['domain_labels']
+  
+  return train_features, domain_labels
+
+# Train Data with image labels
+Train_features, image_labels = load_data(True)
+print(Train_features.shape)
+print(image_labels.shape)
+print(f'Number of training samples: {Train_features.shape[0]}')
+
+# Test Data with image labels
+test_features, image_labels = load_data(False)
+print(test_features.shape)
+print(image_labels.shape)
+
+# 5% of train data with domain label
+train_features, domain_labels = load_data_with_domain_label()
+print(train_features.shape)
+print(domain_labels.shape)
