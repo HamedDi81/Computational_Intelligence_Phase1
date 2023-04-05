68a5097329ff5886c9e265e036a56aa83b00a261
Hamed <hamedmasoudi95@gmail.com>
2023-04-05 23:14:50 +0430

    comment load_feature

--- load_features_H.py
+++ load_features_H.py
@@ -26,17 +26,17 @@ def load_data_with_domain_label() -> Tuple[np.ndarray, np.ndarray]:
   return train_features, domain_labels
 
 # Train Data with image labels
-Train_features, image_labels = load_data(True)
-print(Train_features.shape)
-print(image_labels.shape)
-print(f'Number of training samples: {Train_features.shape[0]}')
+# Train_features, image_labels = load_data(True)
+# print(Train_features.shape)
+# print(image_labels.shape)
+# print(f'Number of training samples: {Train_features.shape[0]}')
 
-# Test Data with image labels
-test_features, image_labels = load_data(False)
-print(test_features.shape)
-print(image_labels.shape)
+# # Test Data with image labels
+# test_features, image_labels = load_data(False)
+# print(test_features.shape)
+# print(image_labels.shape)
 
-# 5% of train data with domain label
-train_features, domain_labels = load_data_with_domain_label()
-print(train_features.shape)
-print(domain_labels.shape)
+# # 5% of train data with domain label
+# train_features, domain_labels = load_data_with_domain_label()
+# print(train_features.shape)
+# print(domain_labels.shape)
